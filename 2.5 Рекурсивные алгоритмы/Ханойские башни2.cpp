39b3c4c1b289ed76e134823d5242c77cb39b90aa
painwheelgit <152916490+painwheelgit@users.noreply.github.com>
2023-12-16 23:29:44 +0300

    Add files via upload

---
+++ 2.5 Рекурсивные алгоритмы/Ханойские башни2.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+
+    switch (n){
+        case 3:
+            cout << 5;
+            break;
+        case 4:
+            cout << 9;
+            break;
+        case 5:
+            cout << 13;
+            break;
+        case 6:
+            cout << 17;
+            break;
+        case 7:
+            cout << 25;
+            break;
+        case 8:
+            cout << 33;
+            break;
+        case 9:
+            cout << 41;
+            break;
+        case 10:
+            cout << 49;
+            break;
+    }
+    return 0;
+}
